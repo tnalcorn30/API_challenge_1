e05d1e7648f0f389d0f25415ab974f3362be5fef
Tiffany Alcorn <tnalcorn390@gmail.com>
2021-04-24 08:18:30 -0400

    first commit for this

---
+++ script.js
@@ -0,0 +1,49 @@
+const baseURL = "https://pokeapi.co/api/v2/pokemon"
+
+// Generate a random Hoenn Pokemon's Pokedex number
+// found this solution at:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function randomNum(min, max) {
+    min = Math.ceil(252);
+    max = Math.floor(387);
+    return Math.floor(Math.random() * (387 - 252) + 252); //The maximum is exclusive and the minimum is inclusive
+}
+
+// Set and check the url
+let pocketMonster = (baseURL + "/" + randomNum())
+console.log(pocketMonster)
+
+// Fetch the data
+function doTheThing(){
+    fetch(pocketMonster)
+    .then(res => {
+        console.log(res)
+        return res.json()
+    })
+    .then(json => {
+        console.log(json)
+        // create a container for the data I'm after
+        let pokeObj = {
+            number: json.id,
+            name: json.name,
+            abilities: {
+                default: json.abilities[0],
+                hidden: json.abilities[1]
+            },
+            // gotta account for that second type not always being applicable at some point
+            elementalType: {
+                type1: json.types[0],
+                type2: json.types[1]
+            }
+        }
+        // get image
+        let pImg = json.sprites.other['official-artwork']
+        console.log(pokeObj)
+    })
+}
+
+// Try to play with the HTML stuffs
+const button = document.querySelector('button')
+
+// The magic button!
+button.addEventListener('click', doTheThing)
