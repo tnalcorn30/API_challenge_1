70806c3c81dfe38d20b6f80c5f8844a5978c37fc
Tiffany Alcorn <tnalcorn390@gmail.com>
2021-04-24 23:51:21 -0400

    slightly updated, no visible change to the page though

--- script.js
+++ script.js
@@ -13,42 +13,52 @@ function randomNum(min, max) {
 let pocketMonster = (baseURL + "/" + randomNum())
 console.log(pocketMonster)
 
+// Try to play with the HTML stuff
+const button = document.querySelector("button")
+let t1Span = document.getElementById("type1Span")
+let t2Span = document.getElementById("type2Span")
+let dexSpan = document.getElementById("dexNumberSpan")
+let nSpan = document.getElementById("nameSpan")
+let dAbSpan = document.getElementById("defAbilitySpan")
+let hAbSpan = document.getElementById("hidAbilitySpan")
+//let cImg = document.getElementById("currentImg")
+
+
 // Fetch the data
-function doTheThing(){
+function doTheThing() {
     fetch(pocketMonster)
-    .then(res => {
-        console.log(res)
-        return res.json()
-    })
-    .then(json => {
-        console.log(json)
-        // create a container for the data I'm after
-        let pokeObj = {
-            number: json.id,
-            name: json.name,
-            abilities: {
-                default: json.abilities[0],
-                hidden: json.abilities[1]
-            },
-            // gotta account for that second type not always being applicable at some point
-            elementalType: {
-                type1: json.types[0],
-                type2: json.types[1]
+        .then(res => {
+            console.log(res)
+            return res.json()
+        })
+        .then(json => {
+            console.log(json)
+            // create a container for the data I'm after
+            let pokeObj = {
+                number: json.id,
+                name: json.name,
+                abilities: {
+                    default: json.abilities[0],
+                    hidden: json.abilities[1]
+                },
+                // gotta account for that second type not always being applicable at some point
+                elementalType: {
+                    type1: json.types[0],
+                    type2: json.types[1]
+                },
+                image: json.sprites.other['official-artwork']
             }
-        }
-        // get image
-        let pImg = json.sprites.other['official-artwork']
-        console.log(pokeObj)
-    })
+            // get image
+            console.log(pokeObj)
+        })
 }
 
-// function to display what the thing did, obviously not done
-async function showTheThings(){
-    let cardGroup = document.createElement(cardGroup);
-}
+// function to update the DOM and show the information retrieved
+async function showTheThings() {
 
-// Try to play with the HTML stuffs
+}
 
 // The magic button!
-const button = document.querySelector('button')
 button.addEventListener('click', doTheThing, showTheThings)
+
+// Didn't actually get it working, but I tried.
